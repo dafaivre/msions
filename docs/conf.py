72d75c915da757f65b6cf0b210f76da1076fe497
Danielle Faivre <dfaivre@uw.edu>
2023-01-05 11:34:29 -0800

    Remove execution timeout

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,10 @@ autoapi_dirs = ["../src"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Notebook execution configuration
+jupyter_execute_notebooks = "cache"
+nb_execution_timeout = -1
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
